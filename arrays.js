e6611f1aa0f6d447eaf932a7b10d4a7295a160d4
AleksaHrevtosva <hrevtosva@yahoo.com>
2020-09-27 22:10:03 +0300

    first task of arrays added

---
+++ arrays.js
@@ -0,0 +1,24 @@
+// new-task-1
+
+const strings = [
+  "As usual, a pilot put off immediately, and rounding the Château d’If, got on board the vessel between Cape Morgiou and Rion island.",
+  "The ship drew on and had safely passed the strait, which some volcanic shock has made between the Calasareigne and Jaros islands; had doubled Pomègue, and approached the harbor under topsails, jib, and spanker, but so slowly and sedately that the idlers, with that instinct which is the forerunner of evil, asked one another what misfortune could have happened on board.",
+  "The vague disquietude which prevailed among the spectators had so much affected one of the crowd that he did not await the arrival of the vessel in harbor, but jumping into a small skiff, desired to be pulled alongside the Pharaon, which he reached as she rounded into La Réserve basin.",
+  "When the young man on board saw this person approach, he left his station by the pilot, and, hat in hand, leaned over the ship’s bulwarks.",
+  "He was a fine, tall, slim young fellow of eighteen or twenty, with black eyes, and hair as dark as a raven’s wing; and his whole appearance bespoke that calmness and resolution peculiar to men accustomed from their cradle to contend with danger."
+]
+
+// 1. Напиши функцию, которая принимает массив строк в качестве параметра.
+// Итерируемую строку из массива храни в переменной с именем stringб
+// полученные значения количества символов сохрани в переменную chars,
+// значения количества слов - в words.
+// Используя шаблонную строку и интерполяцию, выведи в консоль сообщение для каждой строки из массива
+// "Строка "[string]" состоит из [chars] символов и [words] слов"
+
+// 2. Напиши функцию, которая принимает массив строк в качестве параметра
+// и вернет строку, в которой больше всего слов
+
+// 3. Напиши функцию, которая принимает массив строк и искомое слово в качестве параметров
+// и вернет строку, в которой найдет искомое слово из параметра
+// проверь работу функции, передав при вызове поочередно такие слова для поиска: dark, crowd, vessel
+
